d7d0404a07a493887b5e6b7be60f40f828baa256
Jimmy Gosses <gosses.jim@gmail.com>
2018-10-09 20:06:51 -0400

    fill in remainder of greeting sign using js

--- c02/js/example.js
+++ c02/js/example.js
@@ -1,12 +1,45 @@
+// Create variable for the welcome message
 var greeting;
 var name;
 var request;
-var fullMessage;
 
 greeting = 'Howdy ';
 name = 'Molly, ';
 request = 'please check your order';
+
+// Concatenate the three variables to create the welcome message
+var fullMessage;
+
 fullMessage = greeting + name + request;
 
+// Create variables to hold details about the sign
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+// Get the element that has an id of greeting
 var el = document.getElementById('greeting');
+// Replace the content of that element with the personalized welcome message
 el.textContent = fullMessage;
+
+// Get the element that has an id of userSign then update its contents
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+// Get the element that has an id of tiles then update its contents
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+// Get the element that has an id of subTotal then update its contents
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+// Get the element that has an id of shipping then update its contents
+var elShipping = document.getElementById('shipping');
+elShipping.textContent = '$' + shipping;
+
+// Get the element that has an id of grandTotal then updates its contents
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent = '$' + grandTotal;
