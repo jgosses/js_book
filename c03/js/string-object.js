3d467a83d1d4fd0084f98e14fc867a2102158b3f
Jimmy Gosses <gosses.jim@gmail.com>
2018-11-06 19:58:04 -0500

    fix missing single quote

--- c03/js/string-object.js
+++ c03/js/string-object.js
@@ -1,5 +1,5 @@
 var saying = 'Home sweet home ';
-var msg = '<h2>length</h2><p>' + saying.length + '</p>;
+var msg = '<h2>length</h2><p>' + saying.length + '</p>';
 msg += '<h2>uppercase</h2><p>' + saying.toUpperCase() + '</p>';
 msg += '<h2>lowercase</h2><p>' + saying.toLowerCase() + '<p>';
 msg += '<h2>character index: 12</h2><p>' + saying.charAt(12) + '</p>';
